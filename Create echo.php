0fcf87c98bde1d9a6afb55b87174182c3db68eb1
LikhithaTR <156263018+LikhithaTR@users.noreply.github.com>
2024-02-16 13:36:27 -0500

    Create Create echo.php

---
+++ Create echo.php
@@ -0,0 +1,8 @@
+<?php
+
+// Set the appropriate headers to indicate that the response is JavaScript
+header('Content-Type: application/javascript');
+
+// Output the JavaScript code
+echo "alert('Hi');";
+?>
